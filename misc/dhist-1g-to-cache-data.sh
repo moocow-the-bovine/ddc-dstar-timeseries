2557174fccdda9c4a88d133b4ba13256cbf049f8
Bryan Jurish <jurish@bbaw.de>
2020-05-25 12:26:02 +0000

    git-svn-id: file:///home/svn/dev/ddc-dstar-timeseries/trunk@32623 4a515de8-beb3-11dc-84d6-2b654028afac

--- misc/dhist-1g-to-cache-data.sh
+++ misc/dhist-1g-to-cache-data.sh
@@ -3,4 +3,6 @@
 infile="$1"; shift
 [ -z "$infile" ] && infile="-"
 
-tt-cut.awk '$1,$3,$4' "$infile" | tt-1grams.perl -e=utf8 -union -lexsort "$@"
+tt-cut.awk '$1,$3,$4' "$infile" \
+    | perl -ne 'print if (!/\t$/);' \
+    | tt-1grams.perl -e=utf8 -union -lexsort "$@"
--- misc/dhist-db-to-cache-data.sh
+++ misc/dhist-db-to-cache-data.sh
@@ -5,4 +5,6 @@ infile="$1"; shift
 [ -z "$infile" ] && infile="-"
 
 tt-db2dict.perl -of='"$_[1]\t$_[0]"' "$infile" \
-    | tt-cut.awk '$1,$3,$4' - | tt-1grams.perl -e=utf8 -union -lexsort "$@"
+    | tt-cut.awk '$1,$3,$4' - \
+    | perl -ne 'print if (!/\t$/);' \		 
+    | tt-1grams.perl -e=utf8 -union -lexsort "$@"
