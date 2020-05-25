e6c01007e501d3932f5eb00c0169d8dd54f143d8
Bryan Jurish <jurish@bbaw.de>
2020-05-25 12:24:47 +0000

    git-svn-id: file:///home/svn/dev/ddc-dstar-timeseries/trunk@32622 4a515de8-beb3-11dc-84d6-2b654028afac

--- misc/dhist-cache-check.sh
+++ misc/dhist-cache-check.sh
@@ -40,8 +40,13 @@ tmpdir="$1"
 dir0=$(dirname "$0")
 runcmd "$dir0/dhist-json-to-cache-data.sh" "$webdir/dhist-cache.json" -o "$tmpdir/dhist-cache-json.dat" \
     || die "failed to create $tmpdir/dhist-cache-json.dat"
-runcmd "$dir0/dhist-db-to-cache-data.sh" "$webdir/dhist.db" -o "$tmpdir/dhist-cache-db.dat" \
-    || die "failed to create $tmpdir/dhist-cache-db.dat"
+if [ -r "$webdir/dhist.1g" ] ; then
+  runcmd "$dir0/dhist-1g-to-cache-data.sh" "$webdir/dhist.1g" -o "$tmpdir/dhist-cache-db.dat" \
+    || die "failed to create $tmpdir/dhist-cache-db.dat from $webdir/dhist.1g"
+else
+  runcmd "$dir0/dhist-db-to-cache-data.sh" "$webdir/dhist.db" -o "$tmpdir/dhist-cache-db.dat" \
+    || die "failed to create $tmpdir/dhist-cache-db.dat from $webdir/dhist.db"
+fi
 
 rc=0
 if runcmd diff -q "$tmpdir/dhist-cache-json.dat" "$tmpdir/dhist-cache-db.dat" ; then
